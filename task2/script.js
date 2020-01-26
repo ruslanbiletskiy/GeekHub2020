9c13e74e29753bedfdfb191aadbfee26fb59472b
rusbil1987 <rusbil1987@gmail.com>
2020-01-26 19:49:38 +0200

    added cod from lesson2

---
+++ task2/script.js
@@ -0,0 +1,24 @@
+console.log("Завдання 1");
+var a = 0;
+if (a > 0) {
+    console.log(1)
+} else if (a == 0) {
+    console.log(0)
+} else console.log(-1);
+
+console.log("Завдання 2");
+var login, password;
+console.log("who's here");
+login = "Admin";
+if (login == "Cancel") {
+    console.log("canceled")
+} else if (login == "Admin") {
+    console.log("Admin");
+    password = "TheMaster";
+    console.log("password:" + password);
+    if (password == "Cancel") {
+        console.log("Canceled.")
+    } else if (password == "TheMaster") {
+        console.log("Welcome!")
+    } else console.log("Wrong password");
+} else console.log("I don't know you");
